495c34d34edae810ef4bba1189d2d651c56d323f
Andres Felipe Contreras Mendez <contrerandres001@gmail.com>
2025-08-07 17:55:16 -0500

    Update HelloWorld.java

--- Guia_Java/01_Introduccion/HelloWorld.java
+++ Guia_Java/01_Introduccion/HelloWorld.java
@@ -1,1 +1,11 @@
+// HelloWorld.java
+// Este es un ejemplo básico en Java que imprime "Hola, Java!" en la consola.
+// Sirve para entender la estructura mínima de un programa Java.
 
+public class HelloWorld { // Definimos una clase pública llamada HelloWorld
+    // Método principal (punto de entrada del programa)
+    public static void main(String[] args) {
+        // Imprime el texto "Hola, Java!" en la consola
+        System.out.println("Hola, Java!");
+    }
+}
